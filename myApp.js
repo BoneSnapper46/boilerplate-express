c3c987756e5097bef4a72ac9958e9f19c85030bc
BoneSnapper46 <84175607+BoneSnapper46@users.noreply.github.com>
2021-06-19 19:28:50 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+var response = "Hello json".toUpperCase(); // now becomes "HELLO JSON"
 
-
-
+if (process.env.VAR_NAME === "allCaps") {
+  response = "Hello json".toUpperCase();
+} else {
+  response = "Hello json";
+}
 
 
 
